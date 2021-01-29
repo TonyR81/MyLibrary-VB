d56ffb26c502526563dd4eb49b00f92f1405964f
TonyR81 <55560830+TonyR81@users.noreply.github.com>
2021-01-29 14:34:00 +0100

    Creazione classe Employee e relativa interfaccia

---
+++ MyLibrary/Classes/Employee.vb
@@ -0,0 +1,43 @@
+﻿''' <summary>
+''' Employee class
+''' <para>Created by Antonino Razeti on January 29, 2021</para>
+''' <para>Version 1.0</para>
+''' <para> This class contains properties, methods and functions of Employee object</para>
+''' <para>Implements IEmployee</para>
+''' </summary>
+Public Class Employee
+    Inherits Person
+    Implements IEmployee
+
+#Region "Private Declarations"
+
+
+#End Region ' Fine Regions Private Declarations	
+
+#Region "Getters and Setters"
+
+
+#End Region ' End Properties
+
+#Region "Constructors"
+
+
+#End Region ' End Constructors
+
+#Region "Functions"
+
+
+#End Region ' Fine Regione Functions
+
+#Region "Subs"
+
+
+#End Region ' Fine Regions Subs
+
+#Region "Events"
+
+
+#End Region ' Fine Events
+
+
+End Class
---
+++ MyLibrary/Interfaces/IEmployee.vb
@@ -0,0 +1,15 @@
+﻿''' <summary>
+''' Employee interface
+''' <para>Created by Antonino Razeti on January 29, 2021</para>
+''' <para>Version 1.0</para>
+''' <para> Employee Interface that contains all the properties, 
+''' methods and functions of the Employee class</para>
+''' </summary>
+Public Interface IEmployee
+
+#Region "Getters and Setters"
+
+
+#End Region ' End Getters and Setters
+
+End Interface
