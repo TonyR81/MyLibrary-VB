c0f0f04d538223ef4f56226d65e19411ddfd5872
TonyR81 <55560830+TonyR81@users.noreply.github.com>
2021-01-29 15:57:59 +0100

    Creazione classe Customer e relativa interfaccia

---
+++ MyLibrary/Classes/Customer.vb
@@ -0,0 +1,42 @@
+﻿''' <summary>
+''' Customer class
+''' <para>Created by Antonino Razeti on January 27, 2021</para>
+''' <para>Version 1.0</para>
+''' <para> This class contains properties, methods and functions of Customer object</para>
+''' <para>Implements ICustomer</para>
+''' </summary>
+Public Class Customer
+    Inherits Person
+    Implements ICustomer
+
+#Region "Private Declarations"
+
+
+#End Region ' Fine Regions Private Declarations	
+
+#Region "Getters and Setters"
+
+
+#End Region ' End Properties
+
+#Region "Constructors"
+
+
+#End Region ' End Constructors
+
+#Region "Functions"
+
+
+#End Region ' Fine Regione Functions
+
+#Region "Subs"
+
+
+#End Region ' Fine Regions Subs
+
+#Region "Events"
+
+
+#End Region ' Fine Events
+
+End Class
---
+++ MyLibrary/Interfaces/ICustomer.vb
@@ -0,0 +1,15 @@
+﻿''' <summary>
+''' Customer interface
+''' <para>Created by Antonino Razeti on January 27, 2021</para>
+''' <para>Version 1.0</para>
+''' <para> Customer Interface that contains all the properties, 
+''' methods and functions of the Customer class</para>
+''' </summary>
+Public Interface ICustomer
+
+#Region "Getters and Setters"
+
+
+#End Region ' End Getters and Setters
+
+End Interface
