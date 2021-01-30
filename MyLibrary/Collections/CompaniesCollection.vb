52fe4cf30990f7a721c11de12acf6d0e4f879195
TonyR81 <55560830+TonyR81@users.noreply.github.com>
2021-01-30 11:45:16 +0100

    Creazione Companies Collection class e relativa interfaccia

---
+++ MyLibrary/Collections/CompaniesCollection.vb
@@ -0,0 +1,54 @@
+﻿Imports Newtonsoft.Json.Linq
+''' <summary>
+''' Companies Collection class
+''' <para>Created by Antonino Razeti on January 30, 2021</para>
+''' <para>Version 1.0</para>
+''' <para> This class contains properties, methods and functions of Companies Collection object</para>
+''' <para>Implements ICompanies Collection</para>
+''' </summary>
+Public Class CompaniesCollection
+    Inherits UsersCollection
+    Implements ICompaniesCollection
+
+#Region "Private Declarations"
+
+
+#End Region ' Fine Regions Private Declarations	
+
+#Region "Getters and Setters"
+
+
+#End Region ' End Properties
+
+#Region "Constructors"
+
+
+#End Region ' End Constructors
+
+#Region "Functions"
+
+
+#End Region ' Fine Regione Functions
+
+#Region "Subs"
+
+    ''' <summary>
+    ''' Add a range of companies to the current collection given an array of items in json object format
+    ''' </summary>
+    ''' <param name="array">JArray</param>
+    ''' <see cref="JArray"/>
+    Public Overrides Sub AddRange(array As JArray)
+        For index As Integer = 0 To array.Count - 1
+            Items.Add(New Company(array(index - 1).SelectToken("company")))
+        Next
+    End Sub
+
+#End Region ' Fine Regions Subs
+
+#Region "Events"
+
+
+#End Region ' Fine Events
+
+
+End Class
---
+++ MyLibrary/Interfaces/ICompaniesCollection.vb
@@ -0,0 +1,15 @@
+﻿''' <summary>
+''' Copmanies collection interface
+''' <para>Created by Antonino Razeti on January 30, 2021</para>
+''' <para>Version 1.0</para>
+''' <para> Copmanies collection Interface that contains all the properties, 
+''' methods and functions of the Copmanies collection class</para>
+''' </summary>
+Public Interface ICompaniesCollection
+
+#Region "Getters and Setters"
+
+
+#End Region ' End Getters and Setters
+
+End Interface
